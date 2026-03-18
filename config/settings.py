893a09f60f7d8204d1cf436fc648130a91f38c24
OsiyoComputers <hamidovasadbek09@gmail.com>
2026-03-18 11:56:27 +0500

    cors headers added

--- config/settings.py
+++ config/settings.py
@@ -94,10 +94,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
-
 ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
